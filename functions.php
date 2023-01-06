937b52877909bdf963fb93fd6e5c02b6d00aca49
marconagel <marconagel@localhost>
2023-01-06 15:41:10 +0100

    Started coding

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+ * wp connect functions.php file
+ * 
+ * See Theme info in the style.css
+ */
+//we need to die if no ABSPATH defined
+if (!defined('ABSPATH')) { die("Forbidden."); }
+
+//start up
