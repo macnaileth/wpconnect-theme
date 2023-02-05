8e6adab17cfc18f16f53272d869e1d786e6ee80e
marconagel <marconagel@localhost>
2023-02-05 16:06:18 +0100

    Now menus can be added to a JSON string stored as theme option. Will be interesting how much data can be kept this way ;-)

--- js/wpc-adminjs.js
+++ js/wpc-adminjs.js
@@ -9,7 +9,17 @@
 jQuery(document).ready(function($){
     
     //load up needed var for menu creation
-    var structureString = $( '#wpc-menu-structure' ).val();
+    var structureString = $( '#wpc-menu-structure' ).val();    
+    
+    //create new handler object
+    const menuStructure = new wpcStructure( structureString );  
+    
+    //render the stuff - if not empty
+    if ( structureString !== '' ) {
+        $( '.wpc-no-menu' ).hide();
+        $( '#wpc_menu_structure_container' ).html( menuStructure.renderStructure() );
+    }
+    
     console.log( '*** WPC menu structure string loaded: ' + ( structureString.length < 1 ? '- empty -' : structureString ) + ' ***' );
     
     //initalize limited input fields
@@ -49,12 +59,63 @@ jQuery(document).ready(function($){
     });
     //submission
     $( '.wpc-submit-menu' ).on( "click", function(e) {
+   
         //level 1 menu
-        if( $('.wpc-submit-menu').is('#wpc_submit_menu_lvl1') ) {
-            console.log('I am level 1 submit');
-            
+        if( $('.wpc-submit-menu').is('#wpc_submit_menu_lvl1') ) {     
+            structureString = menuStructure.addMenu( $( '#wpc_input_menu_lvl1' ).val() );
+
+            console.log('I am level 1 submit', JSON.parse(structureString));
         }
-        //at the end, always hide modal
+        //at the end, always hide modal and update structure string
         $( '#wpc_menu_modal' ).hide();
+        $( '.wpc-no-menu' ).hide();
+        $( '#wpc-menu-structure' ).val( structureString );
+        
+        //refresh structure and render anew
+        $( '#wpc_menu_structure_container' ).html( menuStructure.renderStructure() );
+        //reset input
+        $( '#wpc_input_menu_lvl1' ).val('');
+        
     } );
 });
+
+class wpcStructure {
+
+    constructor( structure ) {
+        //create json object from retrieved string
+        this.innerStruct = structure == null || structure === '' ? {} : JSON.parse(structure); 
+    }
+    
+    addMenu = ( menuName ) => {
+        //append node to structure json
+        let counter = Object.keys(this.innerStruct).length;
+        
+        if ( counter === 0 ){
+            this.innerStruct['menu-' + 0] = menuName === '' ? {} : { name: menuName };
+        } else {
+            this.innerStruct['menu-' + ( counter )] = menuName === '' ? {} : { name: menuName };
+        }
+        return JSON.stringify( this.innerStruct );   
+    }
+    
+    renderStructure = () => {
+        //display the structure on screen
+        let htmlStr = '';
+        
+        Object.keys( this.innerStruct ).forEach( (key, index) => {
+            
+            let name = this.innerStruct[key].name === undefined || this.innerStruct[key].name === '' ? key : this.innerStruct[key].name + ' (' + key + ')';
+            
+            htmlStr += `<div class="wpc-toplvl-menu" id="${ key }">
+                            <div class="wpc-menu-label">
+                                <span class="wpc_menu-name">${ name }</span>
+                                <span class="wpc-remove">&times;</span>
+                            </div>
+                        </div>
+                        `;
+        });
+        
+        return htmlStr;
+    }
+    
+};
