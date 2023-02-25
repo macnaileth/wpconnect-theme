0d55d549e1fcc2d1201a8abc3d7b086c209a46cd
marconagel <marconagel@localhost>
2023-02-25 17:11:00 +0100

    adding and removing items works now. css = pain in the ass ;-)

--- js/wpc-adminjs.js
+++ js/wpc-adminjs.js
@@ -73,9 +73,26 @@ jQuery(document).ready(function($){
             console.log( '*** WPC: Menu added to structure ***' );
         }
         //level 2 menu item
-        if( submitID === 'wpc_submit_menu_lvl2' ) {     
+        if( submitID === 'wpc_submit_menu_lvl2' ) {  
+            //TODO: Add menu item
+            let itemName = $( "#wpc_item_name" ).val();
+            let itemType = $( "#wpc_item_type" ).val();
             
-            console.log( '*** WPC: Menu item appended to ' + actualMenu + ' ***' );
+            let itemID = -1;
+            //get correct selector value
+            if ( itemType == 0 ){
+                itemID = $( "#wpc_page_id" ).val();
+            } else if ( itemType == 1 ){
+                itemID = $( "#wpc_post_id" ).val();
+            } else if ( itemType == 2 ){
+                itemID = $( "#wpc_cat_id" ).val();
+            } else if ( itemType == 3 ){
+                itemID = $( "#wpc_tag_id" ).val();
+            } else if ( itemType == 4 ){
+                itemID = $( "#wpc_clink_id" ).val();
+            } 
+            
+            structureString = menuStructure.addItem( actualMenu, item = { 'name': itemName, 'type': itemType, 'id': itemID, 'order': 0 } );
         }
         //at the end, always hide modal and update structure string
         $( '#wpc_menu_modal' ).hide();
@@ -89,12 +106,24 @@ jQuery(document).ready(function($){
         
     } );
     //removal - toplevel
-    $( document ).on( "click", '.wpc-toplvl-menu .wpc-remove', function() {
+    $( document ).on( "click", '.wpc-toplvl-menu .wpc-menu-label .wpc-remove', function() {
         //get node to remove
         let node = $( this ).parent().parent().attr('id');
 
         //remove from input string
         structureString = menuStructure.removeMenu( node );
+        //update input field
+        $( '#wpc-menu-structure' ).val( structureString );
+        //visually remove menu from ui
+        $( '#wpc_menu_structure_container' ).html( menuStructure.renderStructure() );
+    });
+    //removal - item
+    $( document ).on( "click", '.wpc-toplvl-menu .wpc-items .wpc-remove', function() {
+        //get node to remove
+        let menu = $( this ).parent().parent().parent().parent().attr('id');
+        let item = $( this ).parent().attr('id');
+        //remove from input string
+        structureString = menuStructure.removeItem( menu, item );
         //update input field
         $( '#wpc-menu-structure' ).val( structureString );
         //visually remove menu from ui
@@ -166,6 +195,31 @@ class wpcStructure {
         return JSON.stringify( this.innerStruct ); 
     }
     
+    removeItem = ( menuName, itemName ) => {  
+        delete this.innerStruct[ menuName ][ itemName ];
+        return JSON.stringify( this.innerStruct ); 
+    }
+    
+    addItem = ( menuName, item = { 'name': '', 'type': 0, 'id': -1, 'order': 0 } ) => {  
+        //count items and append order last
+        let appendOrder = 0;
+        Object.keys( this.innerStruct[ menuName ] ).forEach( ( key ) => { 
+            if ( key !== 'name' ) { 
+                appendOrder += 1;
+            }
+        });
+        item.order = appendOrder;        
+        //type always valid, so we have to check for id and base menu
+        if ( menuName !== '' && item.id != -1 && item.name !== '' ) {         
+            this.innerStruct[ menuName ][ 'item-' + item.type + '-' + item.id ] = item;
+            console.log( '*** WPC: Menu item ' + item.name + ' appended to ' + menuName + ' ***' );
+            return JSON.stringify( this.innerStruct ); 
+        } else {
+            console.log( '*** WPC ERROR: Menu item NOT appended to ' + menuName + ' ***' );
+            return JSON.stringify( this.innerStruct );   
+        }
+    }
+    
     renderStructure = () => {
         //display the structure on screen
         let htmlStr = '';
@@ -179,7 +233,7 @@ class wpcStructure {
                 {}
             );
         
-        Object.keys( orderedOutput ).forEach( (key, index) => {
+        Object.keys( orderedOutput ).forEach( (key) => {
             
             let name = orderedOutput[key].name === undefined || orderedOutput[key].name === '' ? key : orderedOutput[key].name + ' (' + key + ')';
             
@@ -188,7 +242,12 @@ class wpcStructure {
                                 <span class="wpc_menu-name">${ name }</span>
                                 <span class="wpc-remove">&times;</span>
                             </div>
-                            <div class="wpc-menu-body">
+                            <div class="wpc-menu-body"> 
+                                <div class="wpc-items">
+                                    ${ this.renderItems( orderedOutput[key] ) }
+                                </div>
+                            </div>
+                            <div class="wpc-menu-bottom">           
                                 <div class="wpc-item-add">
                                     <a href="#" class="wpc-menu-item-add">${ this.renderSVG() }</a>
                                 </div>
@@ -200,10 +259,71 @@ class wpcStructure {
         return htmlStr;
     }
     
-    renderSVG = ( type = 'PLUS', prefs = { fill: '#8c8f94', stroke: '#8c8f94', strokeWidth: '1' } ) => { 
+    renderItems = ( object ) => {
+        
+        let htmlStr = '';      
+        
+        Object.keys( object ).forEach( ( key ) => { 
+            if ( key !== 'name' ) {
+                   
+                htmlStr += `
+                            <div class="wpc-item" id="${ key }" data-item-order="${ object[key].order }">
+                                <span class="wpc-item-name"><span class="wpc-item-up">&#x2B06;</span><span class="wpc-item-down">&#x2B07;</span> ${ object[key].name } ${ object[key].type == 4 ? '<span class="wpc-link-text">| <a target="_blank" rel="noopener noreferrer" href="//' + this.cleanLink( object[key].id ) + '">' + object[key].id + '</a></span>' : '' }</span>
+                                ${ this.renderItemIcon( object[key].type, object[key].id ) }
+                                <span class="wpc-remove">&times;</span>
+                            </div>           
+                        `; 
+                        
+            }
+        });
+        
+        return htmlStr;
+    }
+    
+    shiftItem = () => {
+        
+    }
+    
+    cleanLink = ( url ) => {
+        //remove protocol if there is
+        let cleanURI = url.replace(/(^\w+:|^)\/\//, '');      
+        //check if is link
+        if ( cleanURI.lastIndexOf('.') === -1 ){
+            return '';
+        } else {
+            return cleanURI;
+        }
+        
+    }
+    
+    renderItemIcon = ( itemType, itemID ) => {
+        
+        let itemPrefs = 'undef';
+        
+        if ( itemType == 0 ){
+            itemPrefs = 'Page';
+        } else if ( itemType == 1 ){
+            itemPrefs = 'Post';
+        } else if ( itemType == 2 ){
+            itemPrefs = 'Category';
+        } else if ( itemType == 3 ){
+            itemPrefs = 'Tag';
+        } else if ( itemType == 4 ){
+            itemPrefs = 'Link';
+        }
+        
+        return `<span class="wpc-item-type wpc-item-${ itemPrefs.toLowerCase() }">
+                    <span class="wpc-item-title">${ itemPrefs }</span>
+                    ${ itemType == 4 ? '' : '<span class="wpc-item-id">' + itemID + '</span>' }
+                </span>`;
+               
+    }
+    
+    
+    renderSVG = ( type = 'PLUS', prefs = { fill: '#8c8f94', stroke: '#8c8f94', strokeWidth: '1', height: '30', width: '30' } ) => { 
         if (type === 'PLUS'){
             return `
-                <svg height="30" width="30">
+                <svg height="${ prefs.height }" width="${ prefs.width }">
                  <polygon points="0,12 12,12 12,0 17,0 17,12 30,12 30,17 17,17 17,30 12,30 12,17 0,17" style="fill:${ prefs.fill };stroke:${ prefs.stroke };stroke-width:${ prefs.strokeWidth }" />.
                 </svg>           
             `;
